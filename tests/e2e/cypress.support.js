ba3eac553166d11ec82582ce9f977602ed85f401
NEDIX <2658411+nedix@users.noreply.github.com>
2023-09-17 07:15:24 +0200

    Update snapshot comparison method

--- tests/e2e/cypress.support.js
+++ tests/e2e/cypress.support.js
@@ -1,9 +1,10 @@
 const { addMatchImageSnapshotCommand } = require('@simonsmith/cypress-image-snapshot/command');
 
 addMatchImageSnapshotCommand({
+  comparisonMethod: 'ssim',
   customDiffDir: Cypress.spec.relative.replace(/^integration\/.*/, 'artifacts/snapshots/'),
   customSnapshotsDir: 'snapshots',
   e2eSpecDir: 'integration',
-  failureThreshold: 0.05,
+  failureThreshold: 0.2,
   failureThresholdType: 'percent',
 });
