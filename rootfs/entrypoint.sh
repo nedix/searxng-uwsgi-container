616b7a5f12b8bf5df8590928f8ccdab719eb763a
NEDIX <2658411+nedix@users.noreply.github.com>
2023-12-28 21:28:44 +0100

    Clean custom theme

--- rootfs/entrypoint.sh
+++ rootfs/entrypoint.sh
@@ -4,5 +4,4 @@ export SEARXNG_SETTINGS_PATH="/etc/searxng/settings.yml"
 
 exec uwsgi --http-socket 0.0.0.0:1234 \
     --ini /usr/local/searxng/dockerfiles/uwsgi.ini \
-    --ini /etc/searxng/uwsgi.ini \
-    --ini /usr/local/searxng/searx/templates/advanced/uwsgi.ini
+    --ini /etc/searxng/uwsgi.ini
